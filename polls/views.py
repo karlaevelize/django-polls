60c51d6a494d1429dad610e74b71e0fb6a86baa9
karlaevelize <k.evelize@gmail.com>
2022-11-05 15:19:43 +0100

    Handling 404

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+# There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). It raises Http404 if the list is empty.
 
 from .models import Question
 
 # Create your views here.
+# def index(request):
+#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#   template = loader.get_template('polls/index.html')
+#   context = { 'latest_question_list': latest_question_list }
+#   # output = ', '.join([q.question_text for q in latest_question_list])
+#   return HttpResponse(template.render(context, request))
+
+# shortcut for view above
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
   context = { 'latest_question_list': latest_question_list }
-  # output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(template.render(context, request))
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+    # question = get_object_or_404(Question, pk=question_id) -> shortcut
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist, from Karla")
+  # return render(request, 'polls/detail.html', {'question': question})
   return HttpResponse("You are looking at questions %s" % question_id)
 
 def results(request, question_id):
