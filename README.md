8bb219265a5c696a89ce865406d387d0d0c49245
karlaevelize <k.evelize@gmail.com>
2022-11-05 13:28:19 +0100

    Models and migrations

--- README.md
+++ README.md
@@ -13,3 +13,25 @@
 5. `cd project-name`
 6. `ls` -> manage.py project-name
 7. `python manage.py runserver`
+
+### Run Migrations
+
+- `python manage.py migrate` -> migrates installed apps
+- `python manage.py makemigrations polls`
+
+output:
+
+```py
+Migrations for 'polls':
+  polls/migrations/0001_initial.py
+    - Create model Question
+    - Create model Choice
+```
+
+- `python manage.py sqlmigrate polls 0001`
+
+### 3-Step Guide to Making Model
+
+1. Change your models `(in models.py)`.
+2. Run `python manage.py makemigrations` to create migrations for those changes
+3. Run `python manage.py migrate` to apply those changes to the database.
--- polls/models.py
+++ polls/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-  question_text = models.CharField(max_length=200)
+  #required to give it a max_length argument, on db schema and validation
+  question_text = models.CharField(max_length=200) 
   pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
+
+## IMPORTANT: To include the app in our project, 
+# we need to add a reference to its configuration 
+# class in the INSTALLED_APPS setting
