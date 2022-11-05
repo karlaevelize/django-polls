6eb79893b39b19cb2c4a88ca03ee89c7184e4054
karlaevelize <k.evelize@gmail.com>
2022-11-05 14:19:34 +0100

    Added admin

--- README.md
+++ README.md
@@ -37,3 +37,10 @@ Migrations for 'polls':
 1. Change your models `(in models.py)`.
 2. Run `python manage.py makemigrations` to create migrations for those changes
 3. Run `python manage.py migrate` to apply those changes to the database.
+
+### Creating admin user
+
+1. `python manage.py createsuperuser`
+2. Followed by username, email and password
+3. Run `python manage.py runserver`
+4. Check at `http://127.0.0.1:8000/admin/`
--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
   #required to give it a max_length argument, on db schema and validation
   question_text = models.CharField(max_length=200) 
   pub_date = models.DateTimeField('date published')
+  def __str__(self):
+    return self.question_text
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
+  def __str__(self):
+    return self.choice_text
 
 ## IMPORTANT: To include the app in our project, 
 # we need to add a reference to its configuration 
